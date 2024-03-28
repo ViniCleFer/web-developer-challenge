a355c203d6d3e26312db91fca356e66948f8eb57
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2024-03-28 20:22:35 -0300

    feat: adding styled-components theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,16 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+
+  'gray-100': '#9F9F9F',
+  'gray-200': '#7A7A7A',
+  'gray-300': '#5F5F5F',
+  'gray-400': '#494949',
+  'gray-500': '#3B3B3B',
+  'gray-600': '#343434',
+  'gray-700': '#2B2B2B',
+  'gray-800': '#313131',
+
+  'green-500': '#70BB02',
+
+  'red-500': '#D65923',
+};
